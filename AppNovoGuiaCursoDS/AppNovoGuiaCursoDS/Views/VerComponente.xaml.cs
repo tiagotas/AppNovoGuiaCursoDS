e7fb90674eae4bff0277e45330aea3b35551ef36
TIAGO SILVA <frost.tiago@gmail.com>
2019-10-08 20:45:05 -0300

    Adicionar arquivos de projeto.

---
+++ AppNovoGuiaCursoDS/AppNovoGuiaCursoDS/Views/VerComponente.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AppNovoGuiaCursoDS.Models;
+
+namespace AppNovoGuiaCursoDS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VerComponente : ContentPage
+    {
+        public VerComponente(Componente c)
+        {
+            InitializeComponent();
+
+            this.BindingContext = c;
+        }
+    }
+}
